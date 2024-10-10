116fdecb00f09665fd77d1dcb6f370d10ad93f75
hiroki-naito <hiroki.n110@gmail.com>
2024-10-10 22:39:37 +0900

    1010-9

--- src/app/api/getnewimage/route.ts
+++ src/app/api/getnewimage/route.ts
@@ -40,7 +40,7 @@ export async function GET(req: NextRequest) {
 
         if (latestFile) {
             // FTP サーバーの画像 URL を生成
-            const imageUrl = `${process.env.FTP_BASE_URL || 'http://example.com/'}cam/${today}/${latestFile.name}`;
+            const imageUrl = `${process.env.FTP_BASE_URL || 'http://example.com/'}cam/${today}/${latestFile.name}?_=${new Date().getTime()}`;
             const filename = latestFile.name;
             return NextResponse.json({ imageUrl, filename, fileList }, { status: 200,
                 headers: {
